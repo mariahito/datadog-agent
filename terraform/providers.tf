578132a7ca50cef675b234e9d113bdb97f4fcb5c
mariahito <mariahito17@gmail.com>
2025-12-30 14:34:22 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  # ADD THIS BLOCK
+  backend "s3" {
+    bucket = "datadog-demo-state" # USE YOUR ACTUAL BUCKET NAME
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
